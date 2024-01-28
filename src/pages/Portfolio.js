38f775e9f21ac7521700811970c912437de525a9
NoelEmmanuel <79769249+NoelEmmanuel@users.noreply.github.com>
2024-01-28 09:08:33 -0600

    final commit

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -3,40 +3,53 @@ import Header from "../components/Header";
 import { useNavigate } from "react-router-dom";
 import TradingViewWidget from "../components/TradingViewWidget";
 import PortfolioTable from "../components/PortfolioTable";
+import React, { useState, useEffect } from "react";
 
 export default function Portfolio() {
+    const [stox, setStox] = useState([]);
+    // useEffect(() => {
+    // fetch("http://127.0.0.1:8000/portfolios/10/", {
+    //     method: "POST"
+    // })
+    // .then((response) => response.json())
+    //     .then((data) => {
+    //         console.log(data);
+    //         localStorage.setItem("stocks", JSON.stringify(data));
+    //     })
+    // }, []);
+
     const stocks = [
         {
             sector: "Communication Services",
             symbol: "FNCM",
             price: 150.25,
-            change: -2.75,
+            change: "Buy",
         },
         {
             sector: "Consumer Discretionary",
             symbol: "FDIV  ",
             price: 62.5,
-            change: 1.2,
+            change: "Buy",
         },
         {
             sector: "Consumer Staples",
             symbol: "FTCG  ",
             price: 62.5,
-            change: 1.2,
+            change: "Buy",
         },
-        { sector: "Energy", symbol: "FSLR", price: 62.5, change: 1.2 },
-        { sector: "Financials", symbol: "FIDS", price: 62.5, change: 1.2 },
-        { sector: "Health Care", symbol: "FHLC", price: 62.5, change: 1.2 },
-        { sector: "Industrials", symbol: "FLIN", price: 62.5, change: 1.2 },
+        { sector: "Energy", symbol: "FSLR", price: 62.5, change: "Sell" },
+        { sector: "Financials", symbol: "FIDS", price: 62.5, change: "Sell" },
+        { sector: "Health Care", symbol: "FHLC", price: 62.5, change: "Buy" },
+        { sector: "Industrials", symbol: "FLIN", price: 62.5, change: "Sell" },
         {
             sector: "Information Technology",
             symbol: "FTEK",
             price: 62.5,
-            change: 1.2,
+            change: "Sell",
         },
-        { sector: "Materials", symbol: "FLMV", price: 62.5, change: 1.2 },
-        { sector: "Real Estate", symbol: "FREL", price: 62.5, change: 1.2 },
-        { sector: "Utilities", symbol: "FLUV", price: 62.5, change: 1.2 },
+        { sector: "Materials", symbol: "FLMV", price: 62.5, change: "Sell" },
+        { sector: "Real Estate", symbol: "FREL", price: 62.5, change: "Buy" },
+        { sector: "Utilities", symbol: "FLUV", price: 62.5, change: "Buy" },
         // Add more stock data as needed
     ];
     const navigate = useNavigate();
